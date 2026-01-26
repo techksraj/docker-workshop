7c7b5df8739c27eb0c654f91fcce654d8c75569d
rajk <tech.ksraj541@gmail.com>
2026-01-25 20:17:08 -0800

    Dockerizing and Ingestion

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import certifi
+import ssl
+import urllib.request
+from tqdm.auto import tqdm
+from sqlalchemy import create_engine
+import click
+
+# Create SSL context with certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+
+
+
+
+
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-password', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--target-table', default='yellow_taxi_data', help='PostgreSQL table')
+def run(year, month, pg_user, pg_password, pg_host, pg_port, pg_db, target_table):
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
+    
+    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=100000,
+    )
+    first = True
+
+    for df_chunk in df_iter:
+
+        if first:
+            # Create table schema (no data)
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="replace"
+            )
+            first = False
+            print("Table created")
+
+        # Insert chunk
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+
+        print("Inserted:", len(df_chunk))
+
+
+
+if __name__ == "__main__":
+    run()
+
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -8,7 +8,9 @@ import pandas as pd
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+#df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+df.to_parquet(f"output_day_1.parquet")
 
 print('hello pipeline')
 
